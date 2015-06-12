3a5c80d99abfefe2cca35dda38bb0228933db1e0
Dan Imbrogno <dan@3vl.ca>
2015-06-12 13:06:09 -0400

    Roughed in social media links

--- public/wp-content/themes/fhac-theme-dev/modules/page-footer.php
+++ public/wp-content/themes/fhac-theme-dev/modules/page-footer.php
@@ -2,47 +2,51 @@
 
 <footer class="page-footer">
 
-	<div class="page-footer--menus">
-		
-		<div class="page-footer--menu">
+	<div class="page-footer--content">
+
+		<div class="page-footer--menus">
 			
-			<?php Menus::MainMenu(); ?>
+			<div class="page-footer--menu">
+				
+				<?php Menus::MainMenu(); ?>
 
-		</div>
+			</div>
 
-		<div class="page-footer--social-menu">
+			<div class="page-footer--social-menu">
 
-			<?php Theme::loadModule('social-menu'); ?>
-
-		</div>
-
-	</div>
-
-	<div class="page-footer--contact">
-		
-		<div class="page-footer--extended-logo-area">
-			
-			<div class="page-footer--extended-logo">
-
-				<span>Forest Hill Animal Clinic</span>
+				<?php Theme::loadModule('social-menu'); ?>
 
 			</div>
 
 		</div>
 
-		<div class="page-footer--reach-out-area">
+		<div class="page-footer--contact">
+			
+			<div class="page-footer--extended-logo-area">
+				
+				<div class="page-footer--extended-logo">
 
-			<?php Theme::loadModule('reach-out'); ?>
+					<span>Forest Hill Animal Clinic</span>
+
+				</div>
+
+			</div>
+
+			<div class="page-footer--reach-out-area">
+
+				<?php Theme::loadModule('reach-out'); ?>
+
+			</div>
 
 		</div>
 
-	</div>
-
-	<div class="page-footer--credits">
-		
-		<div class="page-footer--credits--text">
+		<div class="page-footer--credits">
 			
-			<p>2015 &copy; Forest Hill Animal Clinic  |  Made in Toronto by <a href="http://ballyhoodesign.co">Ballyhoo Graphic Design</a></p>
+			<div class="page-footer--credits--text">
+				
+				<p>2015 &copy; Forest Hill Animal Clinic  |  Made in Toronto by <a href="http://ballyhoodesign.co">Ballyhoo Graphic Design</a></p>
+
+			</div>
 
 		</div>
 
--- src/theme/fhac-theme/modules/social-menu.php
+++ src/theme/fhac-theme/modules/social-menu.php
@@ -1,1 +1,11 @@
-Social Menu
+<ul class="social-menu">
+
+	<li class="social-menu--link social-menu--link--facebook"><a href="#"><span>Facebook</span></a></li>
+	
+	<li class="social-menu--link social-menu--link--twitter"><a href="#"><span>Twitter</span></a></li>
+	
+	<li class="social-menu--link social-menu--link--plus"><a href="#"><span>More</span></a></li>
+	
+	<li class="social-menu--follow-link"><a href="#">Follow</a></li>
+
+</ul>
