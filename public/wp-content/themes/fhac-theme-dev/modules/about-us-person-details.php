c9ccd84552a693a90bee78a5e64a2ae5989b726f
Dan Imbrogno <dan@3vl.ca>
2015-06-23 12:29:10 -0400

    Refines About Us details page

--- public/wp-content/themes/fhac-theme-dev/modules/about-us-person-details.php
+++ public/wp-content/themes/fhac-theme-dev/modules/about-us-person-details.php
@@ -2,39 +2,85 @@
 
 <?php if(!isset($id)) throw new \Exception('Missing ID'); ?>
 
+<?php if(!isset($excerpt)) $excerpt = false ?>
+
 <section class="about-us-person-details">
 			
 	<?php 
 	
 		if($image = get_sub_field('photo')) 
-			printf(
-				'<a href="%s"><img class="about-us-person-details--photo" src="%s" width="%s" height="%s" /></a>',
-				'?field_name='.$field_name.'&id='.$id,
+		{
+			
+			$image_content = sprintf(
+				'<img class="about-us-person-details--photo" src="%s" width="%s" height="%s" />', 
 				$image['sizes'][ImageSizes::SIZE_SQUARE],
 				$image['sizes'][ImageSizes::SIZE_SQUARE.'-width'],
-				$image['sizes'][ImageSizes::SIZE_SQUARE.'-height']); 
+				$image['sizes'][ImageSizes::SIZE_SQUARE.'-height']
+			);
 
-		printf(
-			'<h1 class="about-us-person-details--name"><a href="%s">%s</a></h1>',
-			'?field_name='.$field_name.'&id='.$id,
-			sprintf('%s %s',get_sub_field('first_name'), get_sub_field('last_name'))
-		);
-		
-		if(get_sub_field('credentials')) printf('<h2 class="about-us-person-details--credentials">%s</h2>', get_sub_field('credentials'));
+			if($excerpt == true) {
 
-		if($bio = get_sub_field('bio')) 
-		{
-
-			if($excerpt) {
-			
-				$bio = apply_filters('trim_bio', $bio);
+				$image_content = sprintf('<a href="%s">%s</a>', '?field_name='.$field_name.'&id='.$id, $image_content);
 
 			}
 
-			printf('<p class="about-us-person-details--bio">%s</p>', $bio); 
-
+			printf(
+				'<div class="about-us-person-details--photo-area">%s</div>',
+					$image_content
+				); 
+			
 		}
 
 	?>
+
+	<div class="about-us-person-details--content-area">
+
+		<?php
+
+			$title_content = sprintf(
+				'%s %s',
+				get_sub_field('first_name'),
+				get_sub_field('last_name')
+			);
+
+			if($excerpt == true)
+			{
+
+				$title_content = sprintf(
+					'<a href="%s">%s</a>', 
+					'?field_name='.$field_name.'&id='.$id,
+					$title_content
+				);
+
+			}
+
+			printf('<h1 class="about-us-person-details--name">%s</h1>', $title_content);
+			
+			if(get_sub_field('credentials')) printf('<h2 class="about-us-person-details--credentials">%s</h2>', get_sub_field('credentials'));
+
+			if($bio_content = get_sub_field('bio')) 
+			{
+
+				$read_more_link = '';
+
+				if($excerpt == true) {
+				
+					$bio_content = apply_filters('trim_bio', $bio_content);
+					
+					$bio_content = sprintf(
+						'%s <a class="about-us-person-details--read-more" href="%s"><span>Read More</span></a>',
+						$bio_content, 
+						'?field_name='.$field_name.'&id='.$id
+					);
+
+				}
+
+				printf('<p class="about-us-person-details--bio">%s</p>', $bio_content); 
+
+			}
+
+		?>
+
+	</div>
 
 </section>
